93b87ca2583e777cf89c909efd090b940daf3158
Pyxel <retromannnnn@gmail.com>
2025-06-30 21:22:01 -0400

    drunk

---
+++ Content.Shared/Gaggle/Traits/Assorted/DrunkHiccupComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Gaggle.Traits.Assorted;
+
+/// <summary>
+///     Used for hics while drunk, should automatically add the `DrunkHiccup` tag.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DrunkHiccupComponent : Component
+{
+
+}
